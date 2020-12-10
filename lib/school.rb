7b9fde647d5e575c52c65b8e74732c1e021921fb
“Abbie <abbie.coghlan@gmail.com>
2020-12-09 18:17:29 -0600

    commit tree 8e2d2fff0ec35f08b5c84b4f57ec901296a5df84

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+require "pry"
+
+class School
+   def initialize(name)
+    @name = name
+    @roster = {}
+    end 
+   
+    attr_accessor :name, :roster
+
+    def add_student(student_name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student_name
+    end    
+
+    def grade(grade_number)
+        @roster[grade_number]
+    end 
+   
+    def sort
+        
+        sorted_names = @roster.each do |grade, names|
+            @roster[grade] = names.sort
+        end 
+
+    end
+
+end
